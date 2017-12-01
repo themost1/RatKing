1d0261ece92d8185daea4713a5a91cf8355c7b80
ile123ile <bbravencofounder@gmail.com>
2017-12-01 17:54:56 -0500

    Adding moving platform script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Vector2 start;
+	public Vector2 end;
+	public float rate;
+	private bool direction;
+	private float position;
+
+	// Use this for initialization
+	void Start () {
+		direction = true;
+		position = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (direction)
+		{
+			position += rate;
+		}
+		else
+		{
+			position -= rate;
+		}
+		if (position > 1)
+		{
+			position = 1;
+			direction = false;
+		}
+		else if(position < 0)
+		{
+			position = 0;
+			direction = true;
+		}
+		transform.position = Vector2.Lerp(start, end, position);
+	}
+}
